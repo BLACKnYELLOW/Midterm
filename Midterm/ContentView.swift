1d012a47079d0ec0f20a80898a3bbf82fd7a383a
Yellow <s017787@students.lmsd.org>
2023-01-19 12:05:31 -0500

    Initial Commit

---
+++ Midterm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Midterm
+//
+//  Created by Asher Polsky (student LM) on 1/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Midterm/MidtermApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermApp.swift
+//  Midterm
+//
+//  Created by Asher Polsky (student LM) on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct MidtermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
